c4375758b0a60eb674981b315c71f8241cf8c435
TilliWilli_Home <tilliwilli@gmail.com>
2016-07-18 04:28:11 +0300

    Добавлена музыка + иконки. Работает на Ipad.

---
+++ index.js
@@ -0,0 +1,35 @@
+function ToogleMusic()
+{
+    if($("#musicOff").css("visibility") === "hidden")
+    {
+        $("#musicOn").css("visibility", "hidden");
+        $("#musicOff").css("visibility", "visible");
+        $("#theMusic")[0].pause();
+    }
+    else
+    {
+        $("#musicOn").css("visibility", "visible");
+        $("#musicOff").css("visibility", "hidden");
+        $("#theMusic")[0].play();
+    }
+}
+function StartAutoPlayMusic(pVolumeDelta, pTimeDelta)
+{
+    pVolumeDelta = pVolumeDelta?pVolumeDelta:0.03;
+    pTimeDelta = pTimeDelta?pTimeDelta:100;
+    var startVolume = 0.0;
+    var endVolume = 0.9;
+    var fadeOnTimer;
+    var theMusic = $("#theMusic")[0];
+    theMusic.volume = startVolume;
+    theMusic.play();
+    fadeOnTimer = setInterval(function(){
+        startVolume += pVolumeDelta;
+        if(startVolume >= endVolume)
+        {
+            startVolume = endVolume;
+            clearInterval(fadeOnTimer);
+        }
+        theMusic.volume = startVolume;
+    }, pTimeDelta);
+}
