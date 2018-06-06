cf6c7185a8be9dec145e8ede6fb99735b6715e9b
TilliWilli_Acer2 <tilliwilli@gmail.com>
2018-06-06 23:12:25 +0300

    поправил ссылки

--- index.js
+++ index.js
@@ -1,55 +1,43 @@
-function ToogleMusic()
-{
-    if($("#musicOff").css("visibility") === "hidden")
-    {
+function ToogleMusic() {
+    if ($("#musicOff").css("visibility") === "hidden") {
         $("#musicOn").css("visibility", "hidden");
         $("#musicOff").css("visibility", "visible");
         $("#theMusic")[0].pause();
     }
-    else
-    {
+    else {
         $("#musicOn").css("visibility", "visible");
         $("#musicOff").css("visibility", "hidden");
         $("#theMusic")[0].play();
     }
 }
-function StartAutoPlayMusic(pVolumeDelta, pTimeDelta)
-{
-    pVolumeDelta = pVolumeDelta?pVolumeDelta:0.03;
-    pTimeDelta = pTimeDelta?pTimeDelta:100;
+function StartAutoPlayMusic(pVolumeDelta, pTimeDelta) {
+    pVolumeDelta = pVolumeDelta ? pVolumeDelta : 0.03;
+    pTimeDelta = pTimeDelta ? pTimeDelta : 100;
     var startVolume = 0.0;
     var endVolume = 0.9;
     var fadeOnTimer;
     var theMusic = $("#theMusic")[0];
     theMusic.volume = startVolume;
     theMusic.play();
-    fadeOnTimer = setInterval(function(){
+    fadeOnTimer = setInterval(function () {
         startVolume += pVolumeDelta;
-        if(startVolume >= endVolume)
-        {
+        if (startVolume >= endVolume) {
             startVolume = endVolume;
             clearInterval(fadeOnTimer);
         }
         theMusic.volume = startVolume;
     }, pTimeDelta);
 }
-function GotoFacebook()
-{
-    window.location.href = "https://www.facebook.com/thetilliwilli";
+
+function OpenNewWindow(url, isTab) {
+    if (isTab)
+        window.open(url, "_blank");
+    else
+        window.location.href = url;
 }
-function GotoVK()
-{
-    window.location.href = "https://vk.com/tilliwilli";
-}
-function GotoInstagram()
-{
-    window.location.href = "https://www.instagram.com/thetilliwilli/";
-}
-function GotoEMail()
-{
-    window.location.href = "mailto:tilliwilli@gmail.com";
-}
-function Github()
-{
-    window.location.href = "https://github.com/thetilliwilli";
-}
+
+$("#GotoFacebook").click((e) => { OpenNewWindow("https://www.facebook.com/thetilliwilli", e.ctrlKey); })
+$("#GotoVK").click((e) => { OpenNewWindow("https://vk.com/tilliwilli", e.ctrlKey); })
+$("#GotoInstagram").click((e) => { OpenNewWindow("https://www.instagram.com/thetilliwilli", e.ctrlKey); })
+$("#GotoEMail").click((e) => { OpenNewWindow("mailto:tilliwilli@gmail.com", e.ctrlKey); })
+$("#GotoGithub").click((e) => { OpenNewWindow("https://github.com/thetilliwilli", e.ctrlKey); })
